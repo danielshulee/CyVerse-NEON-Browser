369a737fc4de84c7901649bf646f32d1c646f10d
Daniel Lee <dantheman6100@gmail.com>
2018-09-03 14:30:16 -0700

    -

--- Server.R
+++ Server.R
@@ -772,15 +772,7 @@ function(input, output, session) {
     download_function(file = '/tmp/data.zip')
   })
   
-  output$download_NEON_general <- downloadHandler(filename = "test.zip", content = function(file) {
-    unlink(x = "/home/danielslee/NEON/*", recursive = TRUE, force = TRUE)
-    zipsByProduct(dpID = Product_ID_general(), site = Field_Site_general(), package = Package_type_general(), check.size = FALSE, savepath = "/home/danielslee/NEON/")
-    stackByTable(filepath = paste0("/home/danielslee/NEON/", Folder_general()), folder = TRUE)
-    file.rename(from = paste0("/home/danielslee/NEON/", Folder_general(), "/stackedFiles"), to = paste0("/home/danielslee/NEON/", Folder_general(), "/", Folder_path_general()))
-    setwd(paste0("/home/danielslee/NEON/", Folder_general(), "/"))
-    zip(zipfile = file, files = Folder_path_general())
-    setwd('/srv/shiny-server/NEON-Hosted-Browser')
-  }
+  output$download_NEON_general <- downloadHandler(filename = "test.zip", content = download_function(file = file), contentType = "application/zip"
   )
   ####—— Download NEON data: specific ####
   observeEvent(eventExpr = input$download_NEON_specific,
