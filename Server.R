7e0546a8de85ff89844e749083c55c413ba6aabf
Daniel Lee <dantheman6100@gmail.com>
2018-09-02 21:25:07 -0700

    download test 2

--- Server.R
+++ Server.R
@@ -734,7 +734,7 @@ function(input, output, session) {
   Date_specific_parts <- reactive(req(strsplit(Date_specific_long(), "-")[[1]]))
   Date_specific <- reactive(req(paste0(Date_specific_parts()[1], "-", Date_specific_parts()[2])))
   Year_AOP <- reactive(req(strsplit(as.character(input$year_AOP), "-")[[1]][1]))
-  Folder_path_general <- reactive(req(paste0("../NEON Downloads/NEON_", Field_Site_general(), "_", Product_ID_middle())))
+  Folder_path_general <- reactive(req(paste0("NEON_", Field_Site_general(), "_", Product_ID_middle())))
   Folder_path_specific <- reactive(req(paste0("../NEON Downloads/NEON_", Field_Site_specific(), "_", Date_specific())))
   ####—— Download NEON data: general####
   # observeEvent(eventExpr = input$download_NEON_general,
@@ -751,8 +751,15 @@ function(input, output, session) {
   #                }
   #              })
   output$download_NEON_general <- downloadHandler(filename = "test.zip", content = function(file) {
-    file.copy(from = '/home/danielslee/test_dir/*', to = file)
-  })
+    unlink(x = "/home/danielslee/NEON*", recursive = TRUE, force = TRUE)
+    zipsByProduct(dpID = Product_ID_general(), site = Field_Site_general(), package = Package_type_general(), check.size = FALSE, savepath = "/home/danielslee/NEON/")
+    stackByTable(filepath = paste0("/home/danielslee/NEON/", Folder_general()), folder = TRUE)
+    file.rename(from = paste0("/home/danielslee/NEON/", Folder_general(), "/stackedFiles"), to = paste0("/home/danielslee/NEON/", Folder_general(), "/", Folder_path_general()))
+    setwd(paste0("/home/danielslee/NEON/", Folder_general(), "/"))
+    zip(zipfile = file, files = Folder_path_general())
+    setwd('/srv/shiny-server/NEON-Hosted-Browser')
+  }
+  )
   ####—— Download NEON data: specific ####
   observeEvent(eventExpr = input$download_NEON_specific,
                handlerExpr = {
